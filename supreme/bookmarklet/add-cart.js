acbd56646c97a917cb2f85e962a8565b6156173a
millay1114 <millay1114@gmail.com>
2016-12-09 16:08:43 +0900

    add bookmarklets

---
+++ supreme/bookmarklet/add-cart.js
@@ -0,0 +1,1 @@
+javascript:a = document.getElementById("size");a.selectedIndex=1;document.getElementsByName("commit")[0].click();setTimeout(function(){location="http://www.supremenewyork.com/shop/cart"},500)
